18acc9c28a6c60cc91ec203a97a01c3e965f4498
Pollyana <pollyanarocha416@gmail.com>
2023-03-22 12:02:19 -0400

    front funcionando uma parte

--- galeria/views.py
+++ galeria/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>Alura Spece</h1><p>Bem vindo ao espaco</p>')
+    return render(request, 'galeria/index.html')
 
