3fb3a0faf7149b525926a46692ed087017fe5a23
pollyanarocha416 <pollyanarocha416@gmail.com>
2023-04-10 08:21:56 -0400

    consumindo do banco de dados

--- galeria/views.py
+++ galeria/views.py
@@ -1,18 +1,12 @@
+from galeria.models import Fotografia
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
 
 def index(request):
-
-
-    dados = {
-        1: {"nome": "Nebulosa de Carina",
-            "legenda": "webbtelecope.org / NASA / James Webb"},
-        2: {"nome": "Galaxia NGC 1079",
-            "legenda": "nasa.org / NASA / Hubble"}
-    }
-    return render(request, 'galeria/index.html', {"cards": dados})
+    fotografias = Fotografia.objects.all()#objtos detro do nosso model
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
