97d1a87b32d75d1db205aad5c1f2d466c371ba26
pollyanarocha416 <pollyanarocha416@gmail.com>
2023-04-10 11:55:39 -0400

    toda vez que alterar o models preciso fazer makemigrations e depois a migrate

--- galeria/models.py
+++ galeria/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class Fotografia(models.Model):                         #nignfica vazia
+class Fotografia(models.Model):           
+    OPCOES_CATEGORIA = [
+        ("NEBULOSA", "Nebulosa"),
+        ("ESTRELA", "Estrela"),
+        ("GALÁXIA", "Galáxia"),
+        ("PLANETA", "Planeta"),
+    ]              #nignfica vazia
     nome = models.CharField(max_length=100, null=False, blank=False)#vai aceitar caractere
     legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
 
