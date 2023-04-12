748d868af7d2c9d39ca86f5cf70c66d22c979082
pollyanarocha416 <pollyanarocha416@gmail.com>
2023-04-12 14:13:18 -0400

    o campo pesquisa funcionando agora

--- setup/settings.py
+++ setup/settings.py
@@ -122,7 +122,13 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'setup/static')
 ]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+#midia
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
