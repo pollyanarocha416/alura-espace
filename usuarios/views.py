6473f39ab9b96deffb825af79dee900e151122c4
pollyanarocha416 <pollyanarocha416@gmail.com>
2023-04-17 14:49:58 -0400

    feitas modifacacoes no forms e login

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from usuarios.forms import LoginForms
 # Create your views here.
 def login(request):
-    return render(request, "usuarios/login.html")
+    form = LoginForms()
+    return render(request, "usuarios/login.html", {"form": form})
 
 def cadastro(request):
     return render(request, "usuarios/cadastro.html")
