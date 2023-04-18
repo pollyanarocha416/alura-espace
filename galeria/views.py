a4ffe73ab228dba0ceeda0c851d4dd23a188fbeb
pollyanarocha416 <pollyanarocha416@gmail.com>
2023-04-18 14:20:56 -0400

    não é posivel acessar o index sem login

--- galeria/views.py
+++ galeria/views.py
@@ -1,10 +1,13 @@
 from galeria.models import Fotografia
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 
 
 
 def index(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuario nao logado")
+        return redirect('login')
     fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)#objtos detro do nosso model
     
     return render(request, 'galeria/index.html', {"cards": fotografias})
@@ -14,7 +17,11 @@ def imagem(request, foto_id):
     return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
 
 def buscar(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "Usuario nao logado")
+        return redirect('login')
     fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
+    
     if "buscar" in request.GET:
         nome_a_buscar = request.GET['buscar']
         if nome_a_buscar:
