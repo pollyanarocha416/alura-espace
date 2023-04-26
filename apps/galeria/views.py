e8504f3575a490d41a18ba46d7b54fd258bf3cfa
pollyanarocha416 <pollyanarocha416@gmail.com>
2023-04-26 11:04:27 -0400

    todos os erros corrigido categorias funcionando

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -19,17 +19,17 @@ def imagem(request, foto_id):
 
 def buscar(request):
     if not request.user.is_authenticated:
-        messages.error(request, "Usuario nao logado")
+        messages.error(request, 'Usuário não logado')
         return redirect('login')
-    fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
-    
+
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
+
     if "buscar" in request.GET:
         nome_a_buscar = request.GET['buscar']
         if nome_a_buscar:
             fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
 
-    return render(request, "galeria/buscar.html", {"cards": fotografias})
-
+    return render(request, "galeria/index.html", {"cards": fotografias})
 
 def nova_imagem(request):
     if not request.user.is_authenticated:
@@ -63,3 +63,8 @@ def deletar_imagem(request, foto_id):
     fotografia.delete()
     messages.success(request, 'Deleção feita com sucesso!')
     return redirect('index')
+
+def filtro(request, categoria):
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True, categoria=categoria)
+
+    return render(request, 'galeria/index.html', {"cards": fotografias})
